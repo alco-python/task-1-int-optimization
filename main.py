cc5674fc92967dfbd42c0669f49ca955639e99e9
putyatam <mike.putyata@gmail.com>
2025-09-17 13:10:34 +0300

    the realization of the task 1 was created

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 if __name__ == "__main__":
-    print("It's test of git push by VS Code")
+    M = N = 0
+    flag_positive = flag_negative = True
+    i = 0
+    while flag_positive or flag_negative:
+        if flag_positive:
+            x_pos = eval(str(i+1))
+            y_pos = eval(str(i+1))
+            flag_positive = id(x_pos) == id(y_pos)
+            if not flag_positive:
+                N = i
+        if flag_negative:
+            x_neg = eval(str(-i-1))
+            y_neg = eval(str(-i-1))
+            flag_negative = id(x_neg) == id(y_neg)
+            if not flag_negative:
+                M = i
+        i += 1
+    
+    print(f"[-{M}; {N}]")
+
+
